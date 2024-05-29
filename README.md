bc36833460598a923404130cbcf5558ed6af3310
krish2592 <sachink9225s@gmail.com>
2024-05-29 15:53:55 +0530

    updated project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Project Setup
+
+### Pre-requisite
+
+1) Maven should be install in the system
+2) Docker should be install in the system
+
+
+1. Run maven clean install to install the packages
+
+```
+ mvn clean install
+```
+
+2. Change to the directory and run the script.
+
+```
+cd benchmark/benchmark-nosql/src/scripts/cassandra/ShopingCart/
+```
+
+```
+./initialize-cassandra-db.sh 
+```
+
+3.  
---
+++ scripts/cassandra/ShopingCart/shopingcart-setup-initial-data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex 
+
+cd "$(dirname ${BASH_SOURCE[0]})/../../../workloads/ShopingCart"
+
+if [[ ! -f project_files.zip ]]; then
+  curl 'https://www.comp.nus.edu.sg/~cs4224/project_files.zip' -L -o project_files.zip
+else
+  echo 'project_files.zip already exists'
+fi
+
+if [[ ! -d project_files ]]; then
+  unzip project_files.zip -d .
+else
+  echo 'project_files.zip already extracted'
+fi
+
+
+
+# mkdir tests/ShopingCart
